9998e425bce07d708588d8736cb1fdba68fc2b6c
Filip <ozdoba089@gmail.com>
2019-09-19 14:49:10 +0200

    No changes

--- Script/Bullet.js
+++ Script/Bullet.js
@@ -1,7 +1,6 @@
 class Bullet extends Phaser.GameObjects.Sprite{
   constructor(config, key){
     super(config.scene, config.x, config.y, config.tileset, config.frame)
-
     config.scene.add.existing(this);
     config.scene.physics.add.existing(this);
 
