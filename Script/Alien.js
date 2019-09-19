9e7f9d0462b284edd3b8eac34e66837aa9955d78
Filip <ozdoba089@gmail.com>
2019-09-19 14:48:49 +0200

    No changes

--- Script/Alien.js
+++ Script/Alien.js
@@ -1,11 +1,15 @@
-class Alien extends Phaser.GameObjects.Sprite{
+class Alien extends Phaser.Physics.Arcade. Sprite{
   constructor(config, scoreValue, color){
     super(config.scene, config.x, config.y,  config.tileset, config.frame)
+
     config.scene.add.existing(this);
     config.scene.physics.add.existing(this);
+    this.physicsBodyType = Phaser.Physics.ARCADE;
     this.color=color;
     this.scoreValue=scoreValue;
-
     this.tint = color;
+
   }
+
+
 }
