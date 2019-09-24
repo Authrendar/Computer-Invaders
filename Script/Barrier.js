e78a00250d9faf53ece76b6ddad441870062de25
Filip <ozdoba089@gmail.com>
2019-09-24 18:24:12 +0200

    Barriers have been added. Barriers protect Player out of bullets.

---
+++ Script/Barrier.js
@@ -0,0 +1,42 @@
+class Barrier extends Phaser.Physics.Arcade.Sprite {
+  constructor(config, color) {
+    super(config.scene, config.x, config.y, config.tileset, config.frame)
+    config.scene.add.existing(this);
+    config.scene.physics.add.existing(this);
+    this.physicsBodyType = Phaser.Physics.ARCADE;
+    this.setDisplaySize(50, 50);
+    this.color = color;
+    this.tint = color;
+    this.counter = 0;
+  }
+
+  setColor(color) {
+    this.color = color;
+    this.tint = color;
+  }
+
+  countHits() {
+    this.counter++;
+
+    switch (this.counter) {
+      case 1:
+        this.setColor(green);
+        break;
+      case 2:
+        this.setColor(yellow);
+        break;
+      case 3:
+        this.setColor(orange);
+        break;
+      case 4:
+        this.setColor(red);
+        break;
+      case 5:
+        this.destroy();
+        break;
+
+    }
+  }
+
+
+}
